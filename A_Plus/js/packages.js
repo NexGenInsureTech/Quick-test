243fb62011e29e9806b2feab4d8464645e642314
Nikash Deka <47028816+nikashdeka@users.noreply.github.com>
2026-08-20 20:30:14 +0530

    Add familyOptions array for package options

---
+++ A_Plus/js/packages.js
@@ -0,0 +1,33 @@
+const familyOptions = [
+
+  {
+    code: "1A",
+    label: "Self"
+  },
+
+  {
+    code: "2A",
+    label: "Self + Spouse"
+  },
+
+  {
+    code: "2A1C",
+    label: "Family of 3"
+  },
+
+  {
+    code: "2A2C",
+    label: "Family of 4"
+  },
+
+  {
+    code: "PARENT1",
+    label: "Single Parent"
+  },
+
+  {
+    code: "PARENT2",
+    label: "Parent Couple"
+  }
+
+];
