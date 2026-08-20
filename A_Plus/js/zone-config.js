0301f3d07715d6518bb0708dd152ea83a5f4b58a
Nikash Deka <47028816+nikashdeka@users.noreply.github.com>
2026-08-20 20:21:48 +0530

    Add zone-config.js with zone definitions

---
+++ A_Plus/js/zone-config.js
@@ -0,0 +1,15 @@
+const zones = [
+
+  {
+      code:"ZONE1",
+      title:"Zone 1",
+      description:"Metro & High Cost Healthcare Cities"
+  },
+
+  {
+      code:"ZONE2",
+      title:"Zone 2",
+      description:"Rest of India"
+  }
+
+];
