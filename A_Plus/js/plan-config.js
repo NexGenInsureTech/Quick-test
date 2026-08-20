fb4e6bd5071dcabb85f243f542a5964b0826d1db
Nikash Deka <47028816+nikashdeka@users.noreply.github.com>
2026-08-20 20:21:23 +0530

    Add plan configuration for subscription plans

---
+++ A_Plus/js/plan-config.js
@@ -0,0 +1,21 @@
+const plans = [
+
+  {
+      code:"silver",
+      title:"Silver",
+      badge:"Essential Protection"
+  },
+
+  {
+      code:"gold",
+      title:"Gold",
+      badge:"Most Popular"
+  },
+
+  {
+      code:"diamond",
+      title:"Diamond",
+      badge:"Premium Protection"
+  }
+
+];
